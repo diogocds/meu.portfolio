b214dab88beb60a3a205ae593d28a20b0535e6f2
Diogo Cunha <163685852+diogocds@users.noreply.github.com>
2024-06-04 18:56:45 -0300

    Add files via upload

--- port.js
+++ port.js
@@ -14,3 +14,18 @@ menu.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
+
+function abrirWhatsapp() {
+    var nome = document.getElementById("nome").value;
+    var telefone = document.getElementById("telefone").value;
+    var email = document.getElementById("email").value;
+    var msg = document.getElementById("msg").value;
+    var url = " https://wa.me/5584999209787?text=" // Seu numero
+      + "*Olá Diogo, Gostaria de Mais Informações*" + "%0a" // Mensagem personalizada
+      + "%0a" // Quebra de linha
+      + "*Nome*: " + nome + "%0a" // Dados do formulário
+      + "*Telefone*: " + telefone + "%0a"
+      + "*E-mail*: " + email + "%0a"
+      + "*Mensagem*: " + msg;
+    window.open(url, '_blank').focus();
+  }
